cfbf89ece3f64e9ba3d235377de6840c059f8a67
Harry Solovay <harrysolovay@gmail.com>
2026-02-07 15:33:19 -0500

    implement clean util

--- repo-util/main.ts
+++ repo-util/main.ts
@@ -3,12 +3,14 @@ import { NodeContext } from "@effect/platform-node"
 import { Effect } from "effect"
 import { argv } from "node:process"
 import { alignCommand } from "./align.ts"
+import { cleanCommand } from "./clean.ts"
 import packageJson from "./package.json" with { type: "json" }
 
 Command.run(
   Command.make("repo-util").pipe(
     Command.withSubcommands([
       alignCommand,
+      cleanCommand,
     ]),
   ),
   {
