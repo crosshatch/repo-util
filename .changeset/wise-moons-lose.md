f609d9573df55e6dcaae340991d316583d5cc809
Harry Solovay <harrysolovay@gmail.com>
2026-02-19 16:08:55 -0500

    get cli version from package.json

---
+++ .changeset/wise-moons-lose.md
@@ -0,0 +1,5 @@
+---
+"@crosshatch/repo-util": patch
+---
+
+get cli version from package.json
--- repo-util/main.ts
+++ repo-util/main.ts
@@ -2,10 +2,10 @@ import { Command } from "@effect/cli"
 import { NodeContext, NodeRuntime } from "@effect/platform-node"
 import { Effect } from "effect"
 import { argv } from "node:process"
-import PackageJson from "./package.json"
 
 import { alignCommand } from "./align.ts"
 import { cleanCommand } from "./clean.ts"
+import PackageJson from "./package.json" with { type: "json" }
 
 Command.run(Command.make("repo-util").pipe(Command.withSubcommands([alignCommand, cleanCommand])), {
   name: "Crosshatch Repo Util",
