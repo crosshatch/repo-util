dbcbc780ca49e6d63d8a9c24ca22377810fda8af
Harry Solovay <harrysolovay@gmail.com>
2026-02-19 14:31:26 -0500

    re-specify version

--- repo-util/main.ts
+++ repo-util/main.ts
@@ -2,22 +2,12 @@ import { Command } from "@effect/cli"
 import { NodeContext, NodeRuntime } from "@effect/platform-node"
 import { Effect } from "effect"
 import { argv } from "node:process"
+import PackageJson from "./package.json"
+
 import { alignCommand } from "./align.ts"
 import { cleanCommand } from "./clean.ts"
-import packageJson from "./package.json" with { type: "json" }
 
-Command.run(
-  Command.make("repo-util").pipe(
-    Command.withSubcommands([
-      alignCommand,
-      cleanCommand,
-    ]),
-  ),
-  {
-    name: "Crosshatch Repo Util",
-    version: packageJson.version,
-  },
-)(argv).pipe(
-  Effect.provide(NodeContext.layer),
-  NodeRuntime.runMain,
-)
+Command.run(Command.make("repo-util").pipe(Command.withSubcommands([alignCommand, cleanCommand])), {
+  name: "Crosshatch Repo Util",
+  version: PackageJson.version,
+})(argv).pipe(Effect.provide(NodeContext.layer), NodeRuntime.runMain)
